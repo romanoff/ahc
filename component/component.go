3c509a2ea473035f9076f0235bd1992c2d913889
Andriy Romanov <judo.ras@gmail.com>
2014-03-21 21:44:50 -0700

    Added component rendering

--- component/component.go
+++ component/component.go
@@ -1,5 +1,21 @@
 package component
 
+import (
+	"bytes"
+	"text/template"
+)
+
+// Ahc component
 type Component struct {
 	Namespace string
+	Requires  []string
+	Css       string
+	Template  *template.Template
+}
+
+// Renders component with data
+func (self *Component) Render(params map[string]interface{}) ([]byte, error) {
+	out := bytes.Buffer{}
+	err := self.Template.Execute(&out, params)
+	return out.Bytes(), err
 }
--- component/component_test.go
+++ component/component_test.go
@@ -2,12 +2,21 @@ package component
 
 import (
 	"testing"
+	"text/template"
 )
 
-func TestComponent(t *testing.T) {
-	c := &Component{Namespace: "goog.ui.Button"}
-	if c.Namespace != "goog.ui.Button" {
-		t.Error("Component namespace has not been saved")
+func TestComponentTemplate(t *testing.T) {
+	tmpl := template.Must(template.New("button").
+		Parse("<div class='button'>{{.name}}</div>"))
+	c := &Component{Namespace: "goog.button", Template: tmpl}
+	params := make(map[string]interface{})
+	params["name"] = "Click me"
+	html, err := c.Render(params)
+	if err != nil {
+		t.Errorf("Expected no error while rendering, but got %v", err)
 	}
-
+	expected := "<div class='button'>Click me</div>"
+	if string(html) != expected {
+		t.Errorf("Expected to get:\n%v\n, but got:\n%v", expected, string(html))
+	}
 }
