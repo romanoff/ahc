4f826210786ab67375b3a3d9824096009397a74e
Andriy Romanov <judo.ras@gmail.com>
2014-03-23 00:06:37 -0700

    Added safe component rendering based on schema

--- component/component.go
+++ component/component.go
@@ -2,6 +2,9 @@ package component
 
 import (
 	"bytes"
+	"errors"
+	"fmt"
+	"github.com/romanoff/ahc/schema"
 	"text/template"
 )
 
@@ -11,11 +14,30 @@ type Component struct {
 	Requires  []string
 	Css       string
 	Template  *template.Template
+	Schema    *schema.Schema
 }
 
 // Renders component with data
 func (self *Component) Render(params map[string]interface{}) ([]byte, error) {
 	out := bytes.Buffer{}
 	err := self.Template.Execute(&out, params)
+	return out.Bytes(), err
+}
+
+func (self *Component) RenderSafe(params map[string]interface{}) ([]byte, error) {
+	if self.Schema == nil {
+		return nil, errors.New(fmt.Sprintf("No schema provided for %v", self.Namespace))
+	}
+	schemaErrors := self.Schema.Validate(params)
+	if len(schemaErrors) != 0 {
+		errorString := ""
+		for _, err := range schemaErrors {
+			errorString += fmt.Sprintf("%v\n", err)
+		}
+		return nil, errors.New(errorString)
+	}
+	filteredParams := self.Schema.GetSchemaParams(params)
+	out := bytes.Buffer{}
+	err := self.Template.Execute(&out, filteredParams)
 	return out.Bytes(), err
 }
--- component/component_test.go
+++ component/component_test.go
@@ -1,6 +1,7 @@
 package component
 
 import (
+	"github.com/romanoff/ahc/schema"
 	"testing"
 	"text/template"
 )
@@ -12,6 +13,45 @@ func TestComponentTemplate(t *testing.T) {
 	params := make(map[string]interface{})
 	params["name"] = "Click me"
 	html, err := c.Render(params)
+	if err != nil {
+		t.Errorf("Expected no error while rendering, but got %v", err)
+	}
+	expected := "<div class='button'>Click me</div>"
+	if string(html) != expected {
+		t.Errorf("Expected to get:\n%v\n, but got:\n%v", expected, string(html))
+	}
+}
+
+func TestRenderSafeNoValidation(t *testing.T) {
+	tmpl := template.Must(template.New("button").
+		Parse("<div class='button'>{{.name}}</div>"))
+	c := &Component{Namespace: "goog.button", Template: tmpl}
+	params := make(map[string]interface{})
+	params["name"] = "Click me"
+	_, err := c.RenderSafe(params)
+	if err == nil {
+		t.Errorf("Expected to get missing schema error for RenderSafe, but got nil")
+	}
+}
+
+func TestRenderSafeValidation(t *testing.T) {
+	tmpl := template.Must(template.New("button").
+		Parse("<div class='button'>{{.name}}</div>{{or .not_in_schema \"\"}}"))
+	c := &Component{
+		Namespace: "goog.button",
+		Template:  tmpl,
+		Schema: &schema.Schema{Fields: []*schema.Field{
+			&schema.Field{Name: "name", Required: true, Type: schema.StringField},
+		}},
+	}
+	params := make(map[string]interface{})
+	_, err := c.RenderSafe(params)
+	if err == nil {
+		t.Errorf("Expected to get missing schema parameter error for RenderSafe, but got nil")
+	}
+	params["name"] = "Click me"
+	params["not_in_schema"] = "This text should not appear in button"
+	html, err := c.RenderSafe(params)
 	if err != nil {
 		t.Errorf("Expected no error while rendering, but got %v", err)
 	}
