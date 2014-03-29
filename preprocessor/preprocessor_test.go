869cae71bf026ad36db2002609fe7888b191bd44
Andriy Romanov <judo.ras@gmail.com>
2014-03-29 16:19:37 -0700

    Updated css preprocessor testing

--- preprocessor/preprocessor_test.go
+++ preprocessor/preprocessor_test.go
@@ -1,17 +1,28 @@
 package preprocessor
 
 import (
+	"io/ioutil"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
 func TestCss(t *testing.T) {
-	content := []byte(".a{color:red;}")
-	css := &Css{Content: content}
-	result, err := css.Get()
-	if err != nil {
-		t.Errorf("Expected to not get error while preprocessing css, but got %v", err)
-	}
-	if string(content) != string(result) {
-		t.Errorf("Expected to get:\n%v\n, but got:\n%v", string(content), string(result))
-	}
+	css := &Css{}
+	filepath.Walk("tests", func(filepath string, f os.FileInfo, err error) error {
+		if !f.IsDir() && path.Ext(filepath) == ".acss" {
+			css.Content, _ = ioutil.ReadFile(filepath)
+			expected, _ := ioutil.ReadFile(strings.Replace(filepath, ".acss", ".css", 1))
+			result, err := css.Get()
+			if err != nil {
+				t.Errorf("%v: Expected to not get error while preprocessing css, but got %v", filepath, err)
+			}
+			if string(expected) != string(result) {
+				t.Errorf("%v: Expected to get:\n%v, but got:\n%v", filepath, string(expected), string(result))
+			}
+		}
+		return nil
+	})
 }
