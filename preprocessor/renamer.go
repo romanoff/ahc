82507422bb46fb76f38d1fb12b8489df459e2a2f
Andriy Romanov <judo.ras@gmail.com>
2014-04-08 23:43:07 -0700

    Added function to get minified class name based on counter number

--- preprocessor/renamer.go
+++ preprocessor/renamer.go
@@ -4,3 +4,23 @@ type Renamer struct {
 	Counter int
 	Classes map[string]string
 }
+
+var validClassSymbols = []byte{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+
+func GetMinifiedCssClass(number int) string {
+	divider := 1
+	numberBytes := []byte{}
+	base := len(validClassSymbols)
+	for {
+		divider = divider * base
+		byteNum := ((number % divider) - (number % (divider / base))) / (divider / base)
+		if byteNum == 0 {
+			if number == 0 {
+				numberBytes = append(numberBytes, validClassSymbols[0])
+			}
+			break
+		}
+		numberBytes = append(numberBytes, validClassSymbols[byteNum])
+	}
+	return string(numberBytes)
+}
