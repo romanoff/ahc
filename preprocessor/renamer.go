acd41cc19b5f32934c93e5204170bea1b0239f84
Andriy Romanov <judo.ras@gmail.com>
2014-04-10 23:49:10 -0700

    Added empty process component func to renamer

--- preprocessor/renamer.go
+++ preprocessor/renamer.go
@@ -1,8 +1,16 @@
 package preprocessor
 
+import (
+	"github.com/romanoff/ahc/component"
+)
+
 type Renamer struct {
 	Counter int
 	Classes map[string]string
+}
+
+func (self *Renamer) ProcessComponent(c *component.Component) (error) {
+	return nil
 }
 
 func (self *Renamer) RenameCss(css *Css) error {
@@ -16,6 +24,7 @@ func (self *Renamer) RenameCss(css *Css) error {
 			self.Classes[class] = GetMinifiedCssClass(self.Counter)
 		}
 	}
+	return nil
 }
 
 var validClassSymbols = []byte{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
