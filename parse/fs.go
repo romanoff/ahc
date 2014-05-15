cacf4cdaf84f3fd35fccdfd6e47c0cdbb64f0b77
Andriy Romanov <judo.ras@gmail.com>
2014-05-15 10:45:37 -0700

    Started working on component parser

---
+++ parse/fs.go
@@ -0,0 +1,13 @@
+package parse
+
+import (
+	"github.com/romanoff/ahc/component"
+)
+
+type Fs struct {
+}
+
+// Parses component when path to css or html file is provided
+func (self *Fs) ParseComponent(path string) (*component.Component, error) {
+	return nil, nil
+}
