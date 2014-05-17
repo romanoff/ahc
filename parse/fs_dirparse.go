14a921b47f9d6f3893e7d8758b966eed1240ae0d
Andriy Romanov <judo.ras@gmail.com>
2014-05-17 16:31:20 -0700

    Added function for parsing templates into pool

--- parse/fs_dirparse.go
+++ parse/fs_dirparse.go
@@ -2,6 +2,7 @@ package parse
 
 import (
 	"github.com/romanoff/ahc/component"
+	"github.com/romanoff/ahc/view"
 	"os"
 	"path/filepath"
 )
@@ -14,6 +15,23 @@ func (self *Fs) ParseIntoPool(pool *component.Pool, dirpath string) error {
 				return err
 			}
 			err = pool.AppendComponent(component)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	return nil
+}
+
+func (self *Fs) ParseIntoTemplatePool(pool *view.Pool, dirpath string) error {
+	filepath.Walk(dirpath, func(path string, f os.FileInfo, err error) error {
+		if !f.IsDir() && filepath.Ext(f.Name()) == ".html" {
+			template, err := self.ParseTemplate(path, dirpath)
+			if err != nil {
+				return err
+			}
+			err = pool.AddTemplate(template)
 			if err != nil {
 				return err
 			}
--- parse/fs_dirparse_test.go
+++ parse/fs_dirparse_test.go
@@ -2,6 +2,7 @@ package parse
 
 import (
 	"github.com/romanoff/ahc/component"
+	"github.com/romanoff/ahc/view"
 	"testing"
 )
 
@@ -11,5 +12,14 @@ func TestParseIntoPool(t *testing.T) {
 	fs.ParseIntoPool(pool, "test_files")
 	if len(pool.Components) != 1 {
 		t.Error("Failed to parse button component from test_files folder into pool")
+	}
+}
+
+func TestParseIntoTemplatePool(t *testing.T) {
+	fs := &Fs{}
+	pool := view.InitPool()
+	fs.ParseIntoTemplatePool(pool, "test_templates")
+	if len(pool.Templates) != 1 {
+		t.Error("Failed to parse template from test_templates folder into pool")
 	}
 }
